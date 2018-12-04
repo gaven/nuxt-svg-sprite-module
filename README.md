19cc075b0daecc0f0d580a24de5b30110a5a0c2d
Gaven Heim <gavenheim@gmail.com>
2018-12-03 16:35:15 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# nuxt-svg-sprite-module
+
+> A quick and dirty way to inline svg sprites in your Nuxt app.
+## Install
+
+```sh
+$ yarn add nuxt-svg-sprite-module
+
+$ npm install nuxt-svg-sprite-module --save
+```
+
+## Configure
+
+In `nuxt.config.js`
+
+```js
+  modules: [
+    'nuxt-svg-sprite-module', {
+      directory: '/assets/icons'
+      options: {
+        // Configuration options:
+        // https://github.com/jkphl/svg-sprite#configuration-basics
+      }
+    }
+  ]
+```
+
+## Usage
+
+This module finds svgs in your specified directory and inlines them in Nuxt's [app template](https://nuxtjs.org/guide/views/#app-template). You can pass [svg-sprite options](https://github.com/jkphl/svg-sprite#configuration-basics) to be merged with the the default configuration options.
+
+By default, icons are prefixed with the word `icon`.
+
+```html
+  <svg>
+    <use xlink:href='#icon-foo'></use>
+  </svg>
+```
+
+In order to tell this module where to inline your sprite you need to include a comment within your `app.html` file. Idealy this comment would proceed the opening `<body>` tag. If an `app.html` file does not exist, one will be generated in its absence.
+
+```html
+<!-- svg-sprite -->
+```
+
+## Contributing
+
+Pull requests and stars are always welcome. For bugs and feature requests, [please create an issue](https://github.com/gaven/nuxt-svg-sprite-module/issues/new)
+
+## Author
+
+#### Gaven Heim
+
+* [github/gaven](https://github.com/gaven)
+* [twitter/gavenheim](https://twitter.com/gavenheim)
---
+++ index.js
@@ -0,0 +1,141 @@
+const chalk = require('chalk')
+const File = require('vinyl')
+const inject = require('inject-snippet')
+const SVGSpriter = require('svg-sprite')
+
+const { join } = require('path')
+const { readdirSync, readFileSync, writeFileSync, existsSync } = require('fs')
+
+// SVG sprite options
+let options = {
+	shape: {
+		id: {
+			generator: 'icon-%s'
+		},
+		transform: ['svgo']
+	},
+	mode: {
+		inline: true,
+		symbol: true
+	}
+}
+
+const injectSprite = (sprite, templatePath) => {
+	// Create HTML string from svg sprite
+	const spriteString = `
+		<div id='svg-defs'>
+			${sprite}
+		</div>
+	`
+	// Get html contents of template file
+	let injectedTemplate
+
+	try {
+		injectedTemplate = readFileSync(templatePath, 'utf8')
+	} catch (err) {
+		console.log(chalk.red(err))
+		return
+	}
+
+	// Return template with injected sprite sheet
+	return inject(injectedTemplate, spriteString, { tag: 'svg-sprite', action: 'replace' })
+}
+
+const createTemplateSprite = (sprite) => {
+	return `
+		<!DOCTYPE html>
+		<html {{ HTML_ATTRS }}>
+			<head>
+				{{ HEAD }}
+				<style>
+					#svg-defs {
+						width: 0;
+						height: 0;
+						overflow: hidden;
+						position: absolute;
+					}
+				</style>
+			</head>
+			<body {{ BODY_ATTRS }}>
+				<div id='svg-defs'>
+					${sprite}
+				</div>
+				{{ APP }}
+			</body>
+		</html>
+	`
+}
+
+
+module.exports = function (moduleOptions) {
+	// Get directory from module options
+	const { directory } = moduleOptions
+
+	// Get svg options from module options
+	const { options: svgSpriteOptions } = moduleOptions
+
+	// Make sure there are svg options
+	if (svgSpriteOptions && Object.keys(svgSpriteOptions).length) {
+		options = Object.assign({}, options, svgSpriteOptions)
+	}
+
+	// Create new instance of SVGSpriter
+	const sprite = new SVGSpriter(options)
+
+
+	// Directory where svgs are stored
+	const svgDirectory = join(process.cwd(), directory)
+
+	// Loop over svgs in directory and add to sprite
+	let svgs
+
+	try {
+		svgs = readdirSync(svgDirectory)
+	} catch(err) {
+		console.log(chalk.red(err))
+		return
+	}
+
+	svgs.forEach((svg) => {
+		const path = `/${svg}`
+		sprite.add(new File({
+			path: join(svgDirectory, path),
+			base: svgDirectory,
+			contents: readFileSync(join(svgDirectory, path))
+		}))
+	})
+
+	sprite.compile((err, result) => {
+
+		// Console.log and return if error
+		if (err) {
+			console.log(chalk.red(err))
+			return
+		}
+
+		// Get sprite contents from vinyl file
+		const { sprite } = result.symbol
+		const data = sprite.contents.toString('utf8')
+
+		let appHtml
+		const templateDir = join(process.cwd(), 'app.html')
+
+		// If template exists inject svg sprite
+		if (existsSync(templateDir)) {
+			appHtml = injectSprite(data, templateDir)
+		// Else create template with svg sprite injected
+		} else {
+			appHtml = createTemplateSprite(data)
+		}
+
+		// Write created file to directory
+		try {
+			writeFileSync('app.html', appHtml)
+			console.log(chalk.green('SVG sprite injected successfully'))
+		} catch(err) {
+			console.log(chalk.red('Error writing file to directory'))
+		}
+	})
+}
+
+module.exports.meta = require('./package.json')
